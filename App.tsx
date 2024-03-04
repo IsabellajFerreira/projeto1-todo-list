8884343026af1e536030998b4b87e07e13662881
IsabellajFerreira <isabellajferreira@hotmail.com>
2024-03-03 23:25:33 -0300

    Adicionando routa EditarTarefa

--- App.tsx
+++ App.tsx
@@ -1,118 +1,53 @@
 import React from 'react';
-import { View, StyleSheet, Text, FlatList, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from "@react-navigation/native";
 
-import { Ionicons } from '@expo/vector-icons';
+import EditarTarefa from './app/screens/Editar';
+import Tarefas from './app/screens/Tarefas';
 
+import { createStackNavigator } from "@react-navigation/stack";
 
-const Tarefas = ({ item, indice, delTarefa, marcarTarefa }) => {
-  return (
-    <TouchableOpacity onPress={() => marcarTarefa(indice)}>
-      <View style={{ marginBottom: 10, backgroundColor: 'white', padding: 10, borderRadius: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Text style={{ textDecorationLine: item.isConcluido ? 'line-through' : null }}>{item.tarefa}</Text>
-        <View style={{ flexDirection: 'row' }}>
-          <TouchableOpacity onPress={() => delTarefa(indice)}>
-            <Ionicons name="trash" size={24} color="red" />
-          </TouchableOpacity>
-
-          {item.isConcluido && (
-            <Ionicons name="checkmark-done" size={24} color="green" />
-          )}
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
-}
-
+const Stack = createStackNavigator();
 
 const App = () => {
-
-  const [tarefas, setTarefas] = React.useState([]);
-  const [tarefa, setTarefa] = React.useState(null);
-
-  function adicionarTarefa() {
-
-    if (tarefa === '') {
-      return alert('Digite uma tarefa');
-    }
-
-    const novaTarefa = {
-      tarefa: tarefa,
-      isConcluido: false
-    }
-
-    setTarefas([...tarefas, novaTarefa]);
-    setTarefa(null);
-  }
-
-  function deletarTarefa(indice) {
-    // copia do array
-    const copiaTarefas = [...tarefas];
-
-    // atualizar item
-    const arrayAtualizado = copiaTarefas.filter((_, index) => index !== indice);
-
-    // atualizar estado
-    setTarefas(arrayAtualizado);
-  }
-
-  function marcarTarefa(indice) {
-    // copia do array
-    const copiaTarefas = [...tarefas];
-
-    // atualizar item
-    copiaTarefas[indice].isConcluido = !copiaTarefas[indice].isConcluido;
-
-    // atualizar estado
-    setTarefas(copiaTarefas);
-
-  }
-
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tarefas do dia</Text>
-
-      <FlatList
-        data={tarefas}
-        renderItem={({ item, index }) => (
-          <Tarefas item={item} indice={index} delTarefa={deletarTarefa} marcarTarefa={marcarTarefa} />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite uma tarefa"
-          value={tarefa}
-          onChangeText={(texto) => setTarefa(texto)}
-        />
-        <TouchableOpacity onPress={() => adicionarTarefa()} style={styles.addBtn}>
-          <Ionicons name="add" size={30} color="#C0C0C0" />
-        </TouchableOpacity>
-      </View>
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Tarefas">
+          <Stack.Screen
+            name="Tarefas"
+            component={Tarefas}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="EditarTarefa"
+            component={EditarTarefa}
+            options={{ headerShown: true }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f0f0f0',
     padding: 20,
-    paddingTop: 90
+    paddingTop: 90,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20
+    marginBottom: 20,
   },
   inputContainer: {
     marginBottom: 20,
-    flexDirection: 'row', //eixo horizontal
-    justifyContent: 'center', // alinhamento horizontal
-    alignItems: 'center', // alinhamento vertical
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 30,
   },
   input: {
@@ -121,7 +56,7 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     paddingHorizontal: 10,
     backgroundColor: 'white',
-    width: '100%'
+    flex: 1, // Adicionado para ocupar o espaço disponível
   },
   addBtn: {
     marginLeft: 10,
@@ -129,8 +64,7 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     padding: 5,
     backgroundColor: 'white',
-  }
+  },
 });
-
 
 export default App; 
